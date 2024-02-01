b44e36a672e22bad8b1e369e437ed97325eed677
OneZ3ro <emilioangelo665@gmail.com>
2024-02-01 11:06:21 +0100

    Aggiunta funzione utente premium

--- exam_project/src/main/java/model/dao/UtenteDao.java
+++ exam_project/src/main/java/model/dao/UtenteDao.java
@@ -14,8 +14,8 @@ import model.bean.UtenteBean;
 import utils.DbConnection;
 
 public class UtenteDao {
-	
-	public List<UtenteBean> findAll(){
+
+	public List<UtenteBean> findAll() {
 		String query = "SELECT * FROM utenti";
 		DbConnection dbConnection = new DbConnection();
 		Connection con = dbConnection.getConnection();
@@ -40,7 +40,7 @@ public class UtenteDao {
 				utente.setFlgCancellato(rs.getBoolean("flg_cancellato"));
 				utenti.add(utente);
 			}
-			
+
 		} catch (SQLException e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -48,13 +48,13 @@ public class UtenteDao {
 		dbConnection.closeConnection(con);
 		return utenti;
 	}
-	
+
 	public UtenteBean findById(Long id) {
 		String query = "SELECT * FROM utenti WHERE id_utente=?";
-		
+
 		DbConnection dbCon = new DbConnection();
 		Connection con = dbCon.getConnection();
-		
+
 		PreparedStatement ps = null;
 		try {
 			ps = con.prepareStatement(query);
@@ -87,46 +87,78 @@ public class UtenteDao {
 		}
 
 		dbCon.closeConnection(con);
-		
+
 		return utente;
 	}
-	
-	public Map<Long, Integer> firstQueryExam (){
+
+	public Map<Long, Integer> firstQueryExam() {
 		List<UtenteBean> allUtenti = this.findAll();
 		String query = "SELECT COUNT(id_utente) AS total FROM utenti_libri WHERE id_utente = ? AND descrizione = 'Acquisto'";
 		Map<Long, Integer> utentiTrovati = new HashMap<>();
-		
 
 		DbConnection dbCon = new DbConnection();
 		Connection con = dbCon.getConnection();
-		
+
 		PreparedStatement ps = null;
 		ResultSet rs;
 		try {
-			for (UtenteBean utente: allUtenti) {
+			for (UtenteBean utente : allUtenti) {
 				ps = con.prepareStatement(query);
 				ps.setLong(1, utente.getIdUtente());
 				rs = ps.executeQuery();
-				//rs.getInt("total");
+				// rs.getInt("total");
 				while (rs.next()) {
 					int res = rs.getInt("total");
 					if (res > 5) {
 						utentiTrovati.put(utente.getIdUtente(), res);
 					}
 				}
-				
+
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		dbCon.closeConnection(con);
-		/* for (Map.Entry<Long, Integer> entry : utentiTrovati.entrySet()) {
-		    System.out.println("Utente id: " + entry.getKey() + " ha comprato " + entry.getValue().toString() + " libri in totale");
-		} */
-		
+		/*
+		 * for (Map.Entry<Long, Integer> entry : utentiTrovati.entrySet()) {
+		 * System.out.println("Utente id: " + entry.getKey() + " ha comprato " +
+		 * entry.getValue().toString() + " libri in totale"); }
+		 */
+
 		return utentiTrovati;
 	}
-	
+
+	public List<UtenteBean> firstQueryExtraExam() {
+		String query = "SELECT ul.id_utente, ul.id_libro, l.prezzo FROM utenti_libri ul Join libri l on l.id_libro = ul.id_libro where l.prezzo > 100 AND ul.id_utente = ? order by ul.id_utente;";
+		List<UtenteBean> utentiTrovati = new ArrayList<>();
+		Map<Long, Integer> utentiPrevQuery = this.firstQueryExam();
+
+		DbConnection dbCon = new DbConnection();
+		Connection con = dbCon.getConnection();
+		PreparedStatement ps = null;
+		ResultSet rs;
+		try {
+
+			for (Map.Entry<Long, Integer> entry : utentiPrevQuery.entrySet()) {
+				ps = con.prepareStatement(query);
+				ps.setLong(1, entry.getKey());
+				rs = ps.executeQuery();
+				UtenteBean utenteFound = this.findById(entry.getKey());
+				if (!rs.next()) {
+					System.out.println("No rows found.");
+				} else {
+						utenteFound.setPremium(true);
+				}
+				utentiTrovati.add(utenteFound);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		dbCon.closeConnection(con);
+
+		return utentiTrovati;
+	}
 
 }
--- exam_project/src/main/java/service/UtenteService.java
+++ exam_project/src/main/java/service/UtenteService.java
@@ -11,7 +11,7 @@ public class UtenteService {
 	
 	UtenteDao ud = new UtenteDao();
 
-	public List<UtenteBean> firstQueryExam () {
+	/* public List<UtenteBean> firstQueryExam () {
 		Map<Long, Integer> utentiTrovati = ud.firstQueryExam();
 		List<UtenteBean> result = new ArrayList<>();
 		for (Map.Entry<Long, Integer> entry : utentiTrovati.entrySet()) {
@@ -21,6 +21,11 @@ public class UtenteService {
 		for (UtenteBean ut : result) {
 			System.out.println(ut);
 		}
+		return result;
+	} */
+	
+	public List<UtenteBean> firstQueryExam () {
+		List<UtenteBean> result = ud.firstQueryExtraExam();
 		return result;
 	}
 }
